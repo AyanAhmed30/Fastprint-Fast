e9f250321e49258e7ada0384c36da8b496772c59
AyanAhmed30 <hafizayanahmed9@gmail.com>
2025-10-15 22:30:38 +0500

    test for frontend ci cd pipeline

--- fastprint-frontend-main/fastprint-frontend-main/src/app/page.js
+++ fastprint-frontend-main/fastprint-frontend-main/src/app/page.js
@@ -88,7 +88,7 @@ const Home = () => {
               WebkitTextFillColor: "transparent",
             }}
           >
-            Fast Print Guys
+            Fast Print Guysssss
           </h1>
           <h2
             className="text-[40px] leading-[48px] font-bold mb-2 animate-fadeInUpDelayed"
